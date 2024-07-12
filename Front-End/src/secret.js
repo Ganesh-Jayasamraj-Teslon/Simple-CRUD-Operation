98c83e16c7725a37be4d9779b9483e7a79e457a1
Ganesh Jayasamraj <ganeshjayasamraj@Ganeshs-MacBook-Air.local>
2024-07-12 11:44:54 +0530

    Work finished

--- Front-End/src/secret.js
+++ Front-End/src/secret.js
@@ -1,3 +1,3 @@
-const SECRET_BACK_END_HOST_URL = process.env.B_E_HOST || "http://192.168.0.88:3000"
+const SECRET_BACK_END_HOST_URL = process.env.B_E_HOST || "http://13.202.62.122:3000"
 
 export default { SECRET_BACK_END_HOST_URL }
